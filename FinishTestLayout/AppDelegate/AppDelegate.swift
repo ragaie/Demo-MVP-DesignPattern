69e46de4804706bea7facf33998c4a0fb571a34f
Ragaie alfy Fahmey <eng.ragaie@gmail.com>
2021-01-27 10:42:07 +0200

    Initial Commit

---
+++ FinishTestLayout/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  FinishTestLayout
+//
+//  Created by Ragaie Alfy on 1/27/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ FinishTestLayout/Scences/Home Screen/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinishTestLayout
+//
+//  Created by Ragaie Alfy on 1/27/21.
+//
+
+import UIKit
+
+class HomeView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
