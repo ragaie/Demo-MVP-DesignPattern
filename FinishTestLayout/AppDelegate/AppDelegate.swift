b547a64413ceb994fb78a086f4355848ed3347a2
Ragaie alfy Fahmey <eng.ragaie@gmail.com>
2021-01-27 11:08:49 +0200

    Setup Launch Screen and Set first app screen.

--- FinishTestLayout/AppDelegate/AppDelegate.swift
+++ FinishTestLayout/AppDelegate/AppDelegate.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
